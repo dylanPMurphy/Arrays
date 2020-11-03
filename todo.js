891b1d2b6658c9486f4e0b522a80a27115b1ce44
dylanPMurphy <dylanmurphydba@gmail.com>
2020-11-02 19:06:30 -0800

    pop front

--- todo.js
+++ todo.js
@@ -20,12 +20,20 @@ var test = [12,12,34,542,35,2];
 var v = 234;
 
 
-console.log(AddToFront(test, v))
+var x = AddToFront(test, v);
+
+console.log(x)
 // Pop Front
 
 //Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
+function popFront(arr){
+    for(var i = 0; i < arr.length-1; i++){
+        arr[i] = arr[i+1];
+    }
+    return arr;
+}
 
-
+console.log(popFront(x));
 // Insert At
 
 // Given an array, index, and additional value, insert the value into array at given index. Do this without using built-in array methods. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
