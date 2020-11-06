1ea9edf1f26f45292cffce9207f574f83668f624
dylanPMurphy <dylanmurphydba@gmail.com>
2020-11-05 20:48:53 -0800

    swapPairs is working

--- todo.js
+++ todo.js
@@ -91,6 +91,19 @@ console.log(x)
 // Swap positions of successive pairs of values of given array. If length is odd, do not change the final element. For [1,2,3,4], return [2,1,4,3]. 
 //For example, change input ["Brendan",true,42] to [true,"Brendan",42]. As with all array challenges, do this without using any built-in array methods.
 
+function swapPairs(arr){
+    //if(arr.length()%2==0){
+        for( var i = 0; i<=arr.length-2; i+=2){
+            temp = arr[i+1];
+            arr[i+1] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    //}
+
+}
+console.log(swapPairs([0,1,2,3,4,5]));
+console.log(swapPairs([0,1,2,3,4,5,6]));
 
 // Remove Duplicates
 
